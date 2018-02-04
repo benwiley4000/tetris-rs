7d672d99bb1a3b4cb082156a9634ed04e7d5de52
Ben Wiley <therealbenwiley@gmail.com>
2018-02-04 11:16:57 -0500

    Drawing green square on red background.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ extern crate sdl2;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::{Texture, TextureCreator};
+use sdl2::rect::Rect;
 use std::time::Duration;
 use std::thread::sleep;
+
+const TEXTURE_SIZE: u32 = 32;
 
 pub fn main() {
     let sdl_context = sdl2::init().expect("SDL initialization failed");
@@ -17,12 +21,23 @@ pub fn main() {
         .build()
         .expect("Failed to create window");
 
-    let mut canvas = window.into_canvas().build()
+    let mut canvas = window.into_canvas()
+        .target_texture()
+        .present_vsync()
+        .build()
         .expect("Failed to convert window into canvas");
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
+    let texture_creator: TextureCreator<_> = canvas.texture_creator();
+
+    let mut square_texture: Texture =
+        texture_creator.create_texture_target(None, TEXTURE_SIZE, TEXTURE_SIZE)
+        .expect("Failed to create target texture");
+
+    canvas.with_texture_canvas(&mut square_texture, |texture| {
+        texture.set_draw_color(Color::RGB(0, 255, 0)); // green
+        texture.clear();
+    }).expect("Could not clear texture color!");
+
     let mut event_pump = sdl_context.event_pump()
         .expect("Failed to get SDL event pump");
 
@@ -36,6 +51,17 @@ pub fn main() {
                 _ => {}
             }
         }
+
+        canvas.set_draw_color(Color::RGB(255, 0, 0)); // red
+        canvas.clear();
+
+        canvas.copy(&square_texture,
+                None,
+                Rect::new(0, 0, TEXTURE_SIZE, TEXTURE_SIZE))
+            .expect("Couldn't copy texture into window");
+
+        canvas.present();
+        
         sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
